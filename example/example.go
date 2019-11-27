b20ebf9431cee636101ce3e1505a63be54d05478
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2019-11-27 19:37:58 +0530

    basic example scripts added

---
+++ example/example.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/m-rec/banner"
+)
+
+func main() {
+	fmt.Println("init")
+	banner.NewBanner()
+	fmt.Println("exit")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/m-rec/45b44fc1b115311e1be0b63b0f6ae90751bf6f74
+module github.com/m-rec/banner
 
 go 1.12
